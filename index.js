6f72a40ec310961cd82d15a5a07a78173d321d34
ANGELA-NYUTU <angie.nimo254@gmail.com>
2026-04-05 09:29:09 +0300

    debugged

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ const userInfo ={
   password : 'admin123'
 }
 function createLoginTracker(userInfo){
- const attemptCount = 0;
- (passwordAttempt) =>{
-  attemptCount++;
+ let attemptCount = 0;
+ return (passwordAttempt) =>{
+  
   if(passwordAttempt === userInfo.password){
     console.log ('You are now logged in to your account');
-  } else {
+  } else { attemptCount++;
     console.log(`The password you entered is incorrect. you now have ${3-attemptCount} attempts left`)
   }
   if (attemptCount >= 3){
     console.log('you have reached the maximum number of login attempts. Please contact your administartor for further assistance.')
   }
- }
+ };
 }
-module.exports = createLoginTracker(userInfo)
+
+
+const login = createLoginTracker(userInfo);
+
+login('wrong1');
+login('wrong2');
+login('admin123');
+
+module.exports = login
