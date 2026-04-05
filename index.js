fddd71dd38fa4ee943a1ee82aff6c566686108ff
ANGELA-NYUTU <angie.nimo254@gmail.com>
2026-04-05 08:56:22 +0300

    login limiter

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-
-
-
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+const userInfo ={
+  username :'admin',
+  password : 'admin123'
+}
+function createLoginTracker(userInfo){
+ const attemptCount = 0;
+ (passwordAttempt) =>{
+  attemptCount++;
+  if(passwordAttempt === userInfo.password){
+    console.log ('You are now logged in to your account');
+  } else {
+    console.log(`The password you entered is incorrect. you now have ${3-attemptCount} attempts left`)
+  }
+  if (attemptCount >= 3){
+    console.log('you have reached the maximum number of login attempts. Please contact your administartor for further assistance.')
+  }
+ }
+}
+module.exports = createLoginTracker(userInfo)
