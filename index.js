1fb49a35f2d0fe83d189fc7a3a7d6094251fce96
ANGELA-NYUTU <angie.nimo254@gmail.com>
2026-04-17 10:02:25 +0300

    updated function

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-const userInfo ={
-  username :'admin',
-  password : 'admin123'
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+
+  return (passwordAttempt) => {
+    // If already locked
+    if (attemptCount >= 3) {
+      return 'Account locked due to too many failed login attempts';
+    }
+
+    // If correct password
+    if (passwordAttempt === userInfo.password) {
+      return 'Login successful';
+    }
+
+    // If wrong password
+    attemptCount++;
+    return `Attempt ${attemptCount}: Login failed`;
+  };
 }
-function createLoginTracker(userInfo){
- let attemptCount = 0;
- return (passwordAttempt) =>{
-  
-  if(passwordAttempt === userInfo.password){
-    console.log ('You are now logged in to your account');
-  } else { attemptCount++;
-    console.log(`The password you entered is incorrect. you now have ${3-attemptCount} attempts left`)
-  }
-  if (attemptCount >= 3){
-    console.log('you have reached the maximum number of login attempts. Please contact your administartor for further assistance.')
-  }
- };
-}
 
-
-const login = createLoginTracker(userInfo);
-
-login('wrong1');
-login('wrong2');
-login('admin123');
-
-module.exports = login
+module.exports = { createLoginTracker };
